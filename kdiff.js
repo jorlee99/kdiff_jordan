a555c75bcec158e6cb2a4db5351b13f39d44b5ad
Jordan Lee <jordan@pantheondesign.ca>
2021-06-02 14:03:52 -0700

    testing opacity change

--- kdiff.js
+++ kdiff.js
@@ -724,7 +724,8 @@ function layerComparison(){
 }
 
 function sliderChange() {
-    document.getElementById("diff-xlink-2-pcb").style.opacity = -parseFloat(document.getElementById("sliderRange").value).toFixed(2)+"%";
+    document.getElementById("diff-xlink-1-pcb").style.opacity = parseFloat(document.getElementById("sliderRange").value).toFixed(2)+"%";
+    document.getElementById("diff-xlink-2-pcb").style.x = -parseFloat(document.getElementById("sliderRange").value).toFixed(2)+"%";
   }
 
 function schematicChangeOnClick(sourceObject){
